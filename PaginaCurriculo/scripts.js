69e45a8758217a022a0daa716dabcc4dfe53aa7d
Rs2chel <rachelmeijom@gmail.com>
2025-08-11 22:32:46 -0300

    atualizacoes segunda pagina

--- PaginaCurriculo/scripts.js
+++ PaginaCurriculo/scripts.js
@@ -1,4 +1,5 @@
+const btnVoltar = document.getElementById('btn-anterior');
 
-// https://pixabay.com/pt/photos/fones-de-ouvido-audio-tecnologia-7520738/
-
-// https://pixabay.com/pt/photos/computador-port%C3%A1til-5673901/
+btnVoltar.addEventListener('click', () => {
+    window.history.back();
+});
