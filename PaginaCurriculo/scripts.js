07322ab204698997db0a51b6e33b59d4fa97f315
Rachel <rachelmeijom@gmail.com>
2025-08-12 11:45:45 -0300

    implementando menu resposivo na segunda pagina

--- PaginaCurriculo/scripts.js
+++ PaginaCurriculo/scripts.js
@@ -3,3 +3,23 @@ const btnVoltar = document.getElementById('btn-anterior');
 btnVoltar.addEventListener('click', () => {
     window.history.back();
 });
+
+const btnMenu = document.getElementById('btn-menu');
+const asideMenu = document.querySelector('aside');
+
+// Criar overlay para fundo escurecido
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.appendChild(overlay);
+
+// Abrir/fechar menu
+btnMenu.addEventListener('click', () => {
+  asideMenu.classList.toggle('menu-ativo');
+  overlay.classList.toggle('ativo');
+});
+
+// Fechar menu ao clicar no overlay
+overlay.addEventListener('click', () => {
+  asideMenu.classList.remove('menu-ativo');
+  overlay.classList.remove('ativo');
+});
